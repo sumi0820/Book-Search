7f69dec4f8ed56d1adcd3a7a8261313622460f2f
Sumiya Ushiro <u.sumi0820@gmail.com>
2020-01-11 10:34:28 +0800

    Updates

---
+++ precache-manifest.4f942f42acfe2f219f7eba415f3cad96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eef693eb54cd35d159847f2947f6d20",
+    "url": "/Book-Search/index.html"
+  },
+  {
+    "revision": "b091576cb136fbdab050",
+    "url": "/Book-Search/static/css/main.b24c2760.chunk.css"
+  },
+  {
+    "revision": "ac32d61c7a887a93d4c2",
+    "url": "/Book-Search/static/js/2.40ddf6ba.chunk.js"
+  },
+  {
+    "revision": "b091576cb136fbdab050",
+    "url": "/Book-Search/static/js/main.907fa64d.chunk.js"
+  },
+  {
+    "revision": "4ed8f12b3c5f3bc82d5b",
+    "url": "/Book-Search/static/js/runtime-main.5cd5c363.js"
+  }
+]);
